fef6a2e327de59ae8251a8b12a259cb485f27292
VEER <birat.adak@gmail.com>
2023-07-14 12:15:57 +0530

    FT2023:360: Implements hook_node_view to show visit counts of the node.

---
+++ web/modules/custom/counter_hook/counter_hook.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Defines a custom hook to show visit count in each node.
+ */
+
+/**
+ * Defines hook for count nodes visits.
+ */
+function hook_counter($current_count, \Drupal\node\NodeInterface $node) {
+  if ($current_count === 1) {
+    \Drupal::messenger()->addMessage(t('This node visited for first time.'));
+  }
+}
+git 
